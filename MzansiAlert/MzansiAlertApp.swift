bbb6f862b6c18088ac1468af1acbc646a20fc9f9
LihleNdlovu1 <lihlendlovu@glucode.com>
2025-07-01 11:55:29 +0200

    Initial Commit

---
+++ MzansiAlert/MzansiAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  MzansiAlertApp.swift
+//  MzansiAlert
+//
+//  Created by Lubelihle Ndlovu on 2025/07/01.
+//
+
+import SwiftUI
+
+@main
+struct MzansiAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MzansiAlertTests/MzansiAlertTests.swift
@@ -0,0 +1,16 @@
+//
+//  MzansiAlertTests.swift
+//  MzansiAlertTests
+//
+//  Created by Lubelihle Ndlovu on 2025/07/01.
+//
+
+import Testing
+
+struct MzansiAlertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
